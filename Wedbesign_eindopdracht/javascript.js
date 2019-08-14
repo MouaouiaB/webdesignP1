0136d1e398cf038d29b5b86d3d1ad6a59490e1dc
Mouaouia Bouhtala <46076720+MouaouiaB@users.noreply.github.com>
2019-08-15 00:02:18 +0200

    Add files via upload

---
+++ Wedbesign_eindopdracht/javascript.js
@@ -0,0 +1,28 @@
+/*---------- voor de switch van de table ------------*/
+function Mouaouia(){
+    document.getElementById("actieplanTable").src="images/table1.png"
+          }
+
+function Frank(){
+    document.getElementById("actieplanTable").src="images/table2.png"
+          }
+
+/*---------- voor SMART van mouaouia ------------- */
+function Mouaouia1() {
+    var x = document.getElementById("lijst1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+/*---------- voor SMART van frank ------------- */
+function Frank1() {
+    var x = document.getElementById("lijst2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
